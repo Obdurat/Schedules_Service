6abb2550cdd18ac5870d09c6241d09bf4bbe9e36
Obdurat <omicron_rpl@hotmail.com>
2023-03-22 19:28:42 -0300

    refactor: Moved Gin Engine creation to a separete method

--- create/main.go
+++ create/main.go
@@ -3,17 +3,13 @@ package main
 import (
 	handlers "github.com/Obdurat/Schedules/handlers"
 	"github.com/Obdurat/Schedules/logs"
-	"github.com/gin-gonic/gin"
 	"github.com/sirupsen/logrus"
 )
 
 var _ = logs.Init()
 
 func main() {
-	ginMode := "release"
-	gin.SetMode(ginMode)
-	r := gin.New()
-	r.Use(gin.Recovery())
+	r := handlers.Router()
 	handlers.Repo.Ping()
 	r.POST("/schedules/:company", handlers.CreateSchedule)
 	if err := r.Run(":8080"); err != nil {
