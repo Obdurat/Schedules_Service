f1e05069775e645946a1a8fe07d7bd0a4cb2f271
Obdurat <omicron_rpl@hotmail.com>
2023-03-26 12:19:24 -0300

    refactor: Changed package name from mongo to repository

--- get-all/handlers/getall-schedules.go
+++ get-all/handlers/getall-schedules.go
@@ -7,24 +7,24 @@ import (
 
 	entity "github.com/Obdurat/Schedules/entity"
 	logs "github.com/Obdurat/Schedules/logs"
-	mongo "github.com/Obdurat/Schedules/mongo"
 	query "github.com/Obdurat/Schedules/query"
+	"github.com/Obdurat/Schedules/repository"
 	"github.com/gin-gonic/gin"
 	"github.com/sirupsen/logrus"
 )
 
-var Repo = mongo.New()
-
 func GetSchedules(c *gin.Context) {
+	defer logs.Elapsed("UpdateSchedule")()
 	cn := c.Param("company")
 	where, err := query.Builder(c.Query("where")); if err != nil {
 		logrus.Errorf("Error reading query: %v", err)
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()}); return
 	}
+	repo := repository.Instance
 	logrus.Warnf("Reading Schedules on %v", cn)
 	var schedules []entity.Schedule
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second); defer cancel()
-	col := Repo.Client.Database(cn).Collection("schedules")
+	col := repo.Database(cn)
 	cursor, e := col.Find(ctx, where); if e != nil {
 		logrus.Errorf("Error Reading schedules on MongoDB: %v %v", cn, e)
 		c.JSON(http.StatusBadRequest, gin.H{"error": e.Error()}); return
@@ -39,6 +39,5 @@ func GetSchedules(c *gin.Context) {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()}); return
 	}
 	defer logrus.Info("Query Complete successfully on ", cn)
-	defer logs.Elapsed("UpdateSchedule")()
 	c.JSON(http.StatusOK, gin.H{"result": schedules}); return
 }
